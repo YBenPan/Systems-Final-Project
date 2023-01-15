5b9d9e90aa58b31a46a0bc7cb00a01df7e82b950
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2023-01-15 16:57:02 -0500

    Number to schema data form processor

--- datatypes.c
+++ datatypes.c
@@ -1,7 +1,11 @@
 #include "datatypes.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <limits.h>
 
 int get_datatype_size(struct datatype * dt){
   static int datatype_sizes[] = DATATYPE_SIZES;
@@ -14,4 +18,91 @@ int get_datatype_size(struct datatype * dt){
     rv = dt->args;
   }
   return rv;
+}
+
+// returns a char * pointer to a memory block of size get_datatype_size, returns NULL (graciously fails) if parsing fails
+char * parse_string_to_data(char * strinput, struct datatype * dt){
+  if(dt->type >= TOTAL_DATATYPE_COUNT){
+    printf("ERROR: parse_string_to_data was passed a datatype with type %d, maximum type value cannot exceed %d, exiting!\n", dt->type, TOTAL_DATATYPE_COUNT);
+    exit(1);
+  }
+  char * o = NULL;
+  // note for numbers:
+  // if overflow / underflow, ERROR, errno set to ERANGE
+  // if string has non-string components, ERROR, errno set to EINVAL
+  // hex numbers and octals NOT SUPPORTED, only decimals
+  // all values are SIGNED
+  if(dt->type == DATATYPE_INT || dt->type == DATATYPE_SHORTINT || dt->type == DATATYPE_TINYINT || dt->type == DATATYPE_LONG){
+    if(strinput != '\0'){
+      char * endptr;
+      errno = 0;
+      long long int rv = strtoll(strinput, &endptr, 10);
+      if(errno != 0){
+        if(**endptr == '\0'){
+          long long int maxval;
+          long long int minval;
+          switch(dt->type){
+            case DATATYPE_INT:
+              maxval = INT_MAX;
+              minval = INT_MIN;
+              break;
+            case DATATYPE_SHORTINT:
+              maxval = SHRT_MAX;
+              minval = SHRT_MIN;
+              break;
+            case DATATYPE_TINYINT:
+              maxval = SCHAR_MAX;
+              minval = SCHAR_MIN;
+              break;
+            case DATATYPE_LONG:
+              maxval = LLONG_MAX;
+              minval = LLONG_MIN;
+              break;
+          }
+          if(rv > maxval || rv < minval){
+            // out of bounds
+            errno = ERANGE;
+            o = NULL;
+          } else {
+            // actually good value
+            // to avoid endianness issues, use typecasting here instead:
+            switch(dt->type){
+              case DATATYPE_INT:
+                int * p = calloc(1, sizeof(int));
+                *p = (int)(rv);
+                o = (char *)(p);
+                break;
+              case DATATYPE_SHORTINT:
+                short * p = calloc(1, sizeof(short));
+                *p = (short)(rv);
+                o = (char *)(p);
+                break;
+              case DATATYPE_TINYINT:
+                signed char * p = calloc(1, sizeof(signed char));
+                *p = (signed char)(rv);
+                o = (char *)(p);
+                break;
+              case DATATYPE_LONG:
+                long long * p = calloc(1, sizeof(long long));
+                *p = rv;
+                o = (char *)(p);
+                break;
+            }
+          }
+        } else {
+          // invalid character somewhere
+          errno = EINVAL;
+          o = NULL;
+        }
+      } else {
+        // error, pass it down
+        o = NULL;
+      }
+    }
+  } else if (dt->type == DATATYPE_FLOAT){
+  } else if (dt->type == DATATYPE_DOUBLE){
+  } else if (dt->type == DATATYPE_CHAR){
+  } else if (dt->type == DATATYPE_TEXT){
+  }
+  return o;
 }
--- datatypes.h
+++ datatypes.h
@@ -6,23 +6,26 @@ struct datatype{
 	int args;
 };
 
-#define TOTAL_DATATYPE_COUNT 7
+#define TOTAL_DATATYPE_COUNT 8
 
 #define DATATYPE_INT 0
-#define DATATYPE_SMALL 1
-#define DATATYPE_CHAR 2
+#define DATATYPE_SMALLINT 1
+#define DATATYPE_TINYINT 2
 #define DATATYPE_LONG 3
 #define DATATYPE_FLOAT 4
 #define DATATYPE_DOUBLE 5
-#define DATATYPE_TEXT 6
+#define DATATYPE_CHAR 6
+#define DATATYPE_TEXT 7
 
 // if size is 0, treats args as sizes
-#define DATATYPE_SIZES {4, 2, 1, 8, 4, 8, 0}
+#define DATATYPE_SIZES {4, 2, 1, 8, 4, 8, 1, 0}
 
-#define DATATYPE_LABELS {"int", "small", "char", "long", "float", "double", "text"}
+#define DATATYPE_LABELS {"int", "smallint", "tinyint", "long", "float", "double", "char", "text"}
 
-#define DATATYPE_REQ_ARGUMENTS {0, 0, 0, 0, 0, 0, 1}
+#define DATATYPE_REQ_ARGUMENTS {0, 0, 0, 0, 0, 0, 0, 1}
 
 int get_datatype_size(struct datatype * dt);
+
+char * parse_string_to_data(char * strinput, struct datatype * dt);
 
 #endif
